b5c0140811cc3ca890ad3c6f85428aef180cc40e
vinicius.batista <vinicius.batista@globaltechoficial.com.br>
2025-04-25 09:31:00 -0300

    Estudos_PHP

---
+++ Estudos_PHP/2- If e Else.php
@@ -0,0 +1,23 @@
+//PHP = Personal home page
+//=== (Compara valor e tipo)
+//== (Compara valor)
+//= (Compara tipo)
+//
+
+
+<?php
+
+$idade = '26';
+
+if($idade == 26 ){
+    echo 'Verdadeiro';
+}
+else{
+    echo 'Falso';
+}
+
+
+
+
+
+?>
---
+++ Estudos_PHP/dados.php
@@ -0,0 +1,8 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  // valida e processa os dados
+  // redireciona para uma página de sucesso ou erro
+}
+
+?>
